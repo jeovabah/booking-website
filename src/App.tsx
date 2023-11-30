5c42c82f3d5301d619abd441680a91c26fb03a38
Jeova Junior <jrjeova@hotmail.com>
2023-11-30 01:36:13 -0300

    Removendo conmentario

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    // <BarberIntro />
     <BrowserRouter>
       <RoutesComponent />
     </BrowserRouter>
-    // <BarberBooking />
   );
 }
 
